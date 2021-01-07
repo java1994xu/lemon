9fb7ac2e3b8e2d52374c75e10455e205745122ed
xubb <1343936275@qq.com>
2021-01-08 00:37:52 +0800

    commit iprint.ink

--- lemon-admin/src/main/java/com/lemon/LemonAdminApplication.java
+++ lemon-admin/src/main/java/com/lemon/LemonAdminApplication.java
@@ -2,10 +2,20 @@ package com.lemon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * // web容器中进行部署,继承SpringBootServletInitializer类，重写父类的configure方法
+ */
 @SpringBootApplication
-public class LemonAdminApplication {
+public class LemonAdminApplication extends SpringBootServletInitializer {
 
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(LemonAdminApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(LemonAdminApplication.class, args);
     }
